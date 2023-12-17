50cac00b8affd6a0d52e71fd07c52b325fa5a509
jnguyen727 <styliss777@gmail.com>
2023-12-17 11:32:45 -0500

    Concatenation strings to display.

--- script.js
+++ script.js
@@ -58,50 +58,118 @@ function operate(firstNumber, secondNumber, operator) {
 }
 
 let calculatorText = document.getElementById('calculatorResults');
+
 let plusKey = document.getElementById('plusKey');
 
 plusKey.addEventListener("click", function() {
-    calculatorText.textContent = '+';
+    calculatorText.textContent = calculatorText.textContent + ' +';
+})
+
+let minusKey = document.getElementById('minusKey');
+
+minusKey.addEventListener("click", function() {
+    calculatorText.textContent = calculatorText.textContent + ' -';
+})
+
+let multiplyKey = document.getElementById('multiplyKey');
+
+multiplyKey.addEventListener("click", function() {
+    calculatorText.textContent = calculatorText.textContent + ' x';
+})
+
+let divideKey = document.getElementById('divideKey');
+
+divideKey.addEventListener("click", function() {
+    calculatorText.textContent = calculatorText.textContent + ' ÷';
 })
 let oneKey = document.getElementById('oneKey');
 
 
 
 oneKey.addEventListener("click", function() {
-    calculatorText.textContent = '1';
+
+    if (calculatorText.textContent.toString().slice(-1) == '+' || 
+    calculatorText.textContent.toString().slice(-1) == '-' || 
+    calculatorText.textContent.toString().slice(-1) == 'x' || 
+    calculatorText.textContent.toString().slice(-1) == '÷') {
+        calculatorText.textContent += ' 1';
+    } else {
+        calculatorText.textContent += '1';
+        firstNumber += calculatorText.textContent;
+    }
+    
 })
 
 let twoKey = document.getElementById('twoKey');
 
 twoKey.addEventListener("click", function() {
-    calculatorText.textContent = '2';
+ if (calculatorText.textContent.toString().slice(-1) == '+' || 
+    calculatorText.textContent.toString().slice(-1) == '-' || 
+    calculatorText.textContent.toString().slice(-1) == 'x' || 
+    calculatorText.textContent.toString().slice(-1) == '÷') {
+        calculatorText.textContent += ' 2';
+    } else {
+        calculatorText.textContent += '2';
+    }
+    
 })
- 
 
 
 let threeKey = document.getElementById('threeKey');
 
 threeKey.addEventListener("click", function() {
-    calculatorText.textContent = '3';
+if (calculatorText.textContent.toString().slice(-1) == '+' || 
+    calculatorText.textContent.toString().slice(-1) == '-' || 
+    calculatorText.textContent.toString().slice(-1) == 'x' || 
+    calculatorText.textContent.toString().slice(-1) == '÷') {
+        calculatorText.textContent += ' 3';
+    } else {
+        calculatorText.textContent += '3';
+    }
+    
 })
 
 let fourKey = document.getElementById('fourKey');
 
 
 fourKey.addEventListener("click", function() {
-    calculatorText.textContent = '4';
+if (calculatorText.textContent.toString().slice(-1) == '+' || 
+    calculatorText.textContent.toString().slice(-1) == '-' || 
+    calculatorText.textContent.toString().slice(-1) == 'x' || 
+    calculatorText.textContent.toString().slice(-1) == '÷') {
+        calculatorText.textContent += ' 4';
+    } else {
+        calculatorText.textContent += '4';
+    }
+    
 })
 
 let fiveKey = document.getElementById('fiveKey');
 
 fiveKey.addEventListener("click", function() {
-    calculatorText.textContent = '5';
+if (calculatorText.textContent.toString().slice(-1) == '+' || 
+    calculatorText.textContent.toString().slice(-1) == '-' || 
+    calculatorText.textContent.toString().slice(-1) == 'x' || 
+    calculatorText.textContent.toString().slice(-1) == '÷') {
+        calculatorText.textContent += ' 5';
+    } else {
+        calculatorText.textContent += '5';
+    }
+    
 })
 
 let sixKey = document.getElementById('sixKey');
 
 sixKey.addEventListener("click", function() {
-    calculatorText.textContent = '6';
+ if (calculatorText.textContent.toString().slice(-1) == '+' || 
+    calculatorText.textContent.toString().slice(-1) == '-' || 
+    calculatorText.textContent.toString().slice(-1) == 'x' || 
+    calculatorText.textContent.toString().slice(-1) == '÷') {
+        calculatorText.textContent += ' 6';
+    } else {
+        calculatorText.textContent += '6';
+    }
+    
 })
 
 
@@ -110,17 +178,40 @@ sixKey.addEventListener("click", function() {
 let sevenKey = document.getElementById('sevenKey');
 
 sevenKey.addEventListener("click", function() {
-    calculatorText.textContent = '7';
+if (calculatorText.textContent.toString().slice(-1) == '+' || 
+    calculatorText.textContent.toString().slice(-1) == '-' || 
+    calculatorText.textContent.toString().slice(-1) == 'x' || 
+    calculatorText.textContent.toString().slice(-1) == '÷') {
+        calculatorText.textContent += ' 7';
+    } else {
+        calculatorText.textContent += '7';
+    }
+    
 })
 
 let eightKey = document.getElementById('eightKey');
 
 eightKey.addEventListener("click", function() {
-    calculatorText.textContent = '8';
+if (calculatorText.textContent.toString().slice(-1) == '+' || 
+    calculatorText.textContent.toString().slice(-1) == '-' || 
+    calculatorText.textContent.toString().slice(-1) == 'x' || 
+    calculatorText.textContent.toString().slice(-1) == '÷') {
+        calculatorText.textContent += ' 8';
+    } else {
+        calculatorText.textContent += '8';
+    }
+    
 })
-
 let nineKey = document.getElementById('nineKey');
 
 nineKey.addEventListener("click", function() {
-    calculatorText.textContent = '9';
+if (calculatorText.textContent.toString().slice(-1) == '+' || 
+    calculatorText.textContent.toString().slice(-1) == '-' || 
+    calculatorText.textContent.toString().slice(-1) == 'x' || 
+    calculatorText.textContent.toString().slice(-1) == '÷') {
+        calculatorText.textContent += ' 9';
+    } else {
+        calculatorText.textContent += '9';
+    }
+    
 })
